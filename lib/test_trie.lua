fc41e48595945f1ff19d47c6eeb60def2fb7f12c
Thomas Conway <thomas.conway@petermac.org>
2018-10-24 10:12:33 +1100

    Make the trie saner.

--- lib/test_trie.lua
+++ lib/test_trie.lua
@@ -2,28 +2,17 @@ local A = require "aesop"
 local T = require "trie"
 local Y = require "types"
 
-local strRadix = function(D)
-    return function(d, k)
-        if d > D then
-            return nil
-        end
-        if k == nil or d > #k then
-            return ''
-        end
-        return string.sub(k, d, d)
+local function strRadix(d, k)
+    if d > #k then
+        return nil
     end
+    return string.sub(k, d, d)
 end
 
-local wordRadix = function(D)
+local function wordRadix()
     local key = nil
     local words = {}
     return function(d, k)
-        if d > D then
-            return nil
-        end
-        if k == nil then
-            return ''
-        end
         if key ~= k then
             key = nil
             words = {}
@@ -36,7 +25,7 @@ local wordRadix = function(D)
             j = 1
         end
         if d > #words then
-            return ''
+            return nil
         end
         return words[d]
     end
@@ -44,19 +33,20 @@ end
 
 function test_radix()
     local s = A[1]
-    local radix = strRadix(8)
+    local radix = strRadix
     assert(radix(1, s) == 'A')
     assert(radix(3, s) == 's')
     assert(radix(5, s) == 'p')
     assert(radix(7, s) == 'a')
     assert(radix(8, s) == 'n')
-    assert(radix(9, s) == nil)
+    assert(radix(#s, s) == 'n')
+    assert(radix(1+#s, s) == nil)
 end
 
 function test_radix_1()
     local s1 = "The Bear and the Travelers"
     local s2 = "The Beaver"
-    local radix = strRadix(8)
+    local radix = strRadix
     assert(radix(7, s1) == 'a')
     assert(radix(7, s2) == 'a')
     assert(radix(8, s1) == 'r')
@@ -65,14 +55,14 @@ end
 
 function test_trie_1()
     local s = A[1]
-    local radix = strRadix(8)
+    local radix = strRadix
     local t = T.Trie:create(radix)
     t:insert(s, 42)
     assert(t:find(s) == 42)
 end
 
 function test_trie_2()
-    local radix = strRadix(8)
+    local radix = strRadix
     local t = T.Trie:create(radix)
     local n = #A
     for i = 1,n do
@@ -84,7 +74,7 @@ function test_trie_2()
 end
 
 function test_trie_rank_1()
-    local radix = strRadix(8)
+    local radix = strRadix
     local t = T.Trie:create(radix)
     local n = #A
     for i = 1,n do
@@ -99,30 +89,17 @@ end
 
 function test_word_radix_1()
     local s = "The Bear and the Travelers"
-    local radix = wordRadix(4)
-    assert(radix(1, s) == 'The')
-    assert(radix(2, s) == 'Bear')
-    assert(radix(3, s) == 'and')
-    assert(radix(4, s) == 'the')
-    assert(radix(5, s) == nil)
-end
-
-function test_word_radix_2()
-    local s = "The Bear and the Travelers"
-    local radix = wordRadix(8)
+    local radix = wordRadix()
     assert(radix(1, s) == 'The')
     assert(radix(2, s) == 'Bear')
     assert(radix(3, s) == 'and')
     assert(radix(4, s) == 'the')
     assert(radix(5, s) == 'Travelers')
-    assert(radix(6, s) == '')
-    assert(radix(7, s) == '')
-    assert(radix(8, s) == '')
-    assert(radix(9, s) == nil)
+    assert(radix(6, s) == nil)
 end
 
 function test_trie_rank_2()
-    local radix = wordRadix(4)
+    local radix = wordRadix()
     local t = T.Trie:create(radix)
     local n = #A
     for i = 1,n do
@@ -141,5 +118,4 @@ test_trie_1()
 test_trie_2()
 test_trie_rank_1()
 test_word_radix_1()
-test_word_radix_2()
 test_trie_rank_2()
--- lib/trie.lua
+++ lib/trie.lua
@@ -12,15 +12,15 @@ function Trie:create(radix)
 end
 
 function Trie:insert(k, v)
-    return self:nodeInsert(self.root, k, v)
+    return self:nodeInsert(self.root, k, v, 1)
 end
 
 function Trie:find(k)
-    return self:nodeFind(self.root, k)
+    return self:nodeFind(self.root, k, 1)
 end
 
 function Trie:rank(k)
-    return self:nodeRank(self.root, k, 0)
+    return self:nodeRank(self.root, k, 0, 1)
 end
 
 function Trie:select(n)
@@ -31,95 +31,108 @@ function Trie:check()
     return self:nodeCheckCount(self.root)
 end
 
-function Trie:makeNode(depth)
+function Trie:makeNode()
     local node = {}
-    node.depth = depth
     node.count = 0
-    node.toc = nil
-    node.kids = {}
     return node
 end
 
-function Trie:nodeInsert(node, k, v)
-    local r = self.radix(node.depth, k)
+function Trie:nodeInsert(node, k, v, d)
+    local r = self.radix(d, k)
     if r == nil then
+        if node.here == nil then
+            node.here = {}
+            node.hereCount = 0
+        end
         local deltaCount = 0
-        if node.kids[k] == nil then
+        if node.here[k] == nil then
             deltaCount = 1
         end
-        node.kids[k] = v
+        node.here[k] = v
+        node.hereCount = node.hereCount + deltaCount
+        node.hereToc = nil
         node.count = node.count + deltaCount
         return deltaCount
     end
-    if node.kids[r] == nil then
-        node.kids[r] = self:makeNode(node.depth + 1)
+    if node.kids == nil then
+        node.kids = {}
     end
-    local deltaCount = self:nodeInsert(node.kids[r], k, v)
+    if node.kids[r] == nil then
+        node.kids[r] = self:makeNode()
+    end
+    local deltaCount = self:nodeInsert(node.kids[r], k, v, d+1)
     node.count = node.count + deltaCount
-    node.toc = nil
+    node.kidToc = nil
     return deltaCount
 end
 
 function Trie:ensureToc(node)
-    if node.toc ~= nil then
-        return
+    if node.here ~= nil and node.hereToc == nil then
+        node.hereToc = {}
+        for k,v in pairs(node.here) do
+            table.insert(node.hereToc, k)
+        end
+        table.sort(node.hereToc)
     end
-    node.toc = {}
-    for k,v in pairs(node.kids) do
-        table.insert(node.toc, k)
+    if node.kids ~= nil and node.kidToc == nil then
+        node.kidToc = {}
+        for k,v in pairs(node.kids) do
+            table.insert(node.kidToc, k)
+        end
+        table.sort(node.kidToc)
     end
-    table.sort(node.toc)
 end
 
-function Trie:nodeFind(node, k)
-    local r = self.radix(node.depth, k)
+function Trie:nodeFind(node, k, d)
+    local r = self.radix(d, k)
     if r == nil then
-        return node.kids[k]
+        if node.here == nil then
+            return nil
+        end
+        return node.here[k]
     end
-    if node.kids[r] == nil then
+    if node.kids == nil or node.kids[r] == nil then
         return nil
     end
-    return self:nodeFind(node.kids[r], k)
+    return self:nodeFind(node.kids[r], k, d+1)
 end
 
 function Trie:nodeCheckCount(node)
-    local r = self.radix(node.depth)
-    if r == nil then
-        local n = 0
-        for k,v in pairs(node.kids) do
+    local n = 0
+    if node.here ~= nil then
+        for k,v in pairs(node.here) do
             n = n + 1
         end
-        assert(node.count == n)
-        return
+        assert(node.hereCount == n)
     end
-    local n = 0
-    for r0,node0 in pairs(node.kids) do
-        self:nodeCheckCount(node0)
-        n = n + node0.count
+    if node.kids ~= nil then
+        for r0,node0 in pairs(node.kids) do
+            self:nodeCheckCount(node0)
+            n = n + node0.count
+        end
     end
     assert(n == node.count)
 end
 
-function Trie:nodeRank(node, k, n)
-    local r = self.radix(node.depth, k)
+function Trie:nodeRank(node, k, n, d)
     Trie:ensureToc(node)
-    if r == nil then
-        for i = 1,#node.toc do
-            local k0 = node.toc[i]
+    if node.here ~= nil then
+        for i = 1,#node.hereToc do
+            local k0 = node.hereToc[i]
             if k0 >= k then
                 return n
             end
             n = n + 1
         end
-        return n
     end
-    for i = 1,#node.toc do
-        local r0 = node.toc[i]
+    local r = self.radix(d, k)
+    for i = 1,#node.kidToc do
+        local r0 = node.kidToc[i]
         if r0 > r then
             break
         end
         if r0 == r then
-            return self:nodeRank(node.kids[r], k, n)
+            return self:nodeRank(node.kids[r], k, n, d+1)
         end
         local node0 = node.kids[r0]
         n = n + node0.count
@@ -130,14 +143,16 @@ end
 function Trie:nodeSelect(node, n)
     assert(i < node.count)
     Trie:ensureToc(node)
-    local r = self.radix(node.depth)
-    if r == nil then
-        local k = node.toc[n]
-        local v = node.kids[k]
-        return k,v
+    if node.here ~= nil then
+        if n < node.hereCount then
+            local k = node.hereToc[n]
+            local v = node.here[k]
+            return k,v
+        end
+        n = n - here.Count
     end
-    for i = 1,#node.toc do
-        local r0 = node.toc[i]
+    for i = 1,#node.kidToc do
+        local r0 = node.kidToc[i]
         local node0 = node.kids[r0]
         if n < node0.count then
             return Trie:nodeSelect(node, n)
