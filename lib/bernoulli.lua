afb1f672e81d68457bf749b3bb4c7da8c1a03c46
Tom Conway <drtconway@users.noreply.github.com>
2018-10-24 20:00:06 +1100

    Update bernoulli.lua

--- lib/bernoulli.lua
+++ lib/bernoulli.lua
@@ -1,1 +1,17 @@
+local bernoulli = {}
 
+local special = require('special')
+
+function bernoulli.pvalue(vec)
+  local mu = 0
+  local var = 0
+  local x = 0
+  for p,q in pairs(vec) do
+      mu = mu + p
+      var = var + p*(1 - p)
+      x = x + q
+  end
+  return special.gauss(x, mu, math.sqrt(var))
+end
+
+return bernoulli
